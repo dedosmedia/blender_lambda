80ed38bfff7ef37de843dfd55b2ef4bb9f1ae49b
Diego Fernando Díaz Aristiźabal <newton_maxwell@hotmail.com>
2022-12-07 12:50:28 -0500

    refact: blender-store

--- microservices/blender-store/src/app.py
+++ microservices/blender-store/src/app.py
@@ -33,14 +33,39 @@ def validate_event(event):
 
 
 def save_to_ddb(ddb_payload):
+    msg = None
+    valid_flag = True
+
+    try:
+        msg = ddb_client.put_item(
+            TableName=DYNAMODB_TABLE_NAME,
+            Item=ddb_payload,
+            ConditionExpression= 'attribute_not_exists(ResourceId)'
+        )
+        logger.info('DDB Response: {}'.format(msg))
+    except Exception as e:
+        valid_flag = False
+        msg = str(e)
+        logger.info('DDB Error : {}'.format(e))
+
+    return  (valid_flag, msg)
+
+def put_s3(file_content, path):
+    msg = None
+    valid_flag = True
+
+    try:
+        msg = s3_client.put_object(Bucket=BUCKET_NAME, Key=path, Body=file_content)
+        logger.info('S3 Response: {}'.format(msg))
+    except Exception as e:
+        valid_flag = False
+        msg = str(e)
+        logger.info('S3 Error : {}'.format(e))
     
-    return ddb_client.put_item(
-        TableName=DYNAMODB_TABLE_NAME,
-        Item=ddb_payload
-    )
+    return  (valid_flag, msg)
 
 
-def handler_s3(event, context):
+def handler(event, context):
 
     print(event)
     
@@ -53,15 +78,27 @@ def handler_s3(event, context):
         file_content = base64.b64decode(event['body'])
         event_id = event['headers']['event-id']
         session_id = event['headers']['session-id']
-        response = None
+        
 
         resource_id = uuid.uuid4()
+        response = None
         file_extension = pathlib.Path(file_name).suffix.lower()
         path = f'events/{event_id}/{session_id}/{resource_id}{file_extension}'
         
+        # Qué pasa si hay error al subir a S3?
+        s3_validation, msg = put_s3(file_content, path)
+        if s3_validation is False:
+            response = {
+                    'statusCode': 400,
+                    'body': msg,
+                    'headers': {
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*'
+                    }
+                }
+            return(response)
 
         # TODO:  Qué pasa si es duplicado el uuid?
-        
         ddb_payload = {
                     'ResourceId': {
                         'S': str(resource_id)
@@ -79,26 +116,30 @@ def handler_s3(event, context):
                         'S': file_extension
                     }
                 }
-        data = save_to_ddb(ddb_payload)
+        ddb_validation, msg = save_to_ddb(ddb_payload)
 
-        try:
-            s3_response = s3_client.put_object(Bucket=BUCKET_NAME, Key=path, Body=file_content)
-            logger.info('S3 Response: {}'.format(s3_response))
-
-            response  = {
-                'statusCode': 200,
-                'headers': {
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Credentials': 'true'
-                },
-                'body': ''
-            }
-
-            response['body'] = 'Your file has been uploaded'
-            return response
-
-        except Exception as e:
-            raise IOError(e)   
+        
+        
+        if ddb_validation is False:
+            response = {
+                    'statusCode': 400,
+                    'body': msg,
+                    'headers': {
+                        'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*'
+                    }
+                }
+            return(response)   
+         
+        response = {
+            'statusCode': 200,
+            'body': 'successfully created item!',
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+        }
+        return(response)  
     else:
         response = {
                     'statusCode': 400,
@@ -108,3 +149,4 @@ def handler_s3(event, context):
                         'Access-Control-Allow-Origin': '*'
                     }
                 }
+        return(response)
--- microservices/blender-store/variables.tf
+++ microservices/blender-store/variables.tf
@@ -1,4 +1,4 @@
 locals {
     function_name = "blender-storing"
-    image_tag = "0.0.5"
+    image_tag = "0.0.6"
 }
